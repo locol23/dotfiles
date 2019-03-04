198f3c4cdb7bef3519131832f0f55a6590288356
Yoshitaka Terazawa <locol6622@gmail.com>
2019-03-04 21:55:08 +0900

    feat: add vim

--- custom.zsh
+++ custom.zsh
@@ -6,10 +6,9 @@ prompt pure
 export EDITOR=vim
 eval "$(direnv hook zsh)"
 
-#alias
+# alias
 alias g='git'
 alias gbd="g b --merged | grep -vE '^\*|master$|develop$' | xargs -I % git b -d %"
 alias d=docker
 alias dc=docker-compose
-alias vi=nvim
-alias vim=nvim
+alias vi=vim
