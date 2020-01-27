d15d3f313c7791f19a7ada0594d5f9a2a21c3ffc
Yoshitaka Terazawa <locol6622@gmail.com>
2020-01-27 14:22:50 +0900

    feat: add font

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"NotoMono Nerd Font Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
